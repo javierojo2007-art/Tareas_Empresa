5e77b8f998dd635992de213f4b7923547ebcab2c
javierojo2007@gmail.com <javierojo2007@gmail.com>
2026-05-12 18:45:42 +0200

    Clase 05- formulario estudios y array con los estudios

--- script.js
+++ script.js
@@ -21,3 +21,43 @@ botonTema.addEventListener("click", () => {
     }
 
 });
+
+const formularioEstudio = document.getElementById("formulario-estudio");
+const tituloInput = document.getElementById("titulo-estudio");
+const fechaInput = document.getElementById("fecha-estudio");
+const descripcionInput = document.getElementById("descripcion-estudio");
+const contenedorEstudios = document.getElementById("contenedor-estudios");
+const listaEstudios = [];
+
+formularioEstudio.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+    const estudio = {
+
+        titulo: tituloInput.value,
+        fecha: fechaInput.value,
+        descripcion: descripcionInput.value
+
+    };
+
+    listaEstudios.push(estudio);
+    crearTarjetaEstudio(estudio);
+    formularioEstudio.reset();
+
+});
+
+function crearTarjetaEstudio(estudio) {
+
+    const tarjeta = document.createElement("article");
+    tarjeta.classList.add("tarjeta-estudio");
+    const titulo = document.createElement("h3");
+    titulo.textContent = estudio.titulo;
+    const fecha = document.createElement("small");
+    fecha.textContent = estudio.fecha;
+    const descripcion = document.createElement("p");
+    descripcion.textContent = estudio.descripcion;
+    tarjeta.appendChild(titulo);
+    tarjeta.appendChild(fecha);
+    tarjeta.appendChild(descripcion);
+    contenedorEstudios.appendChild(tarjeta);
+}
